8a5d2823021848969a3f940934d63a1cd6dc5ba5
Sai Pratap <pratap@illiquidlabs.com>
2024-05-16 15:02:08 +0530

    as

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useEffect } from "react";
 
+
 function App() {
   const theme = createTheme({
     palette: {
